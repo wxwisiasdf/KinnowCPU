4101925c8a9f232b12d58816d6f8e37f5aecdace
wxwisiasdf <wxwisiasdf@debian-BULLSEYE-live-builder-AMD64>
2022-11-18 22:59:49 -0800

    Add gcc backend (wip)

--- toolchain.sh
+++ toolchain.sh
@@ -1,11 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 export PREFIX="$HOME/opt/cross"
 mkdir -p "$PREFIX"
 mkdir -p build-gcc && cd build-gcc
-[ -f Makefile ] || ../gcc/configure --disable-nls --enable-languages=c \
-	--target=limn2600-mintia --disable-gcov	--disable-multiarch \
+[ -f Makefile ] || ../gcc/configure --disable-nls --enable-languages=c,c++ \
+    --target=limn2600-mintia --disable-gcov	--disable-multiarch \
     --disable-threads --disable-tls --disable-bootstrap \
     --disable-gnu-unique-object --disable-lto --without-headers \
-	--disable-plugin --prefix="$PREFIX" || exit
+    --disable-plugin --prefix="$PREFIX" --enable-checking=all || exit
 make CC="ccache gcc" CXX="ccache g++" all-gcc -j$(nproc) || exit
+if [ "$1" == "install" ]; then
+    make CC="ccache gcc" CXX="ccache g++" install-gcc || exit
+fi
 cd ..
